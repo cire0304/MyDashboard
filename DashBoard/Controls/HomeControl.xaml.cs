bca2792fe3a846b1b1ac6cd5c1acb62a9746bfea
Cire <dongjun.dev@gmail.com>
2025-03-12 20:37:27 +0900

    Move files from views to controls

--- DashBoard/Controls/HomeControl.xaml.cs
+++ DashBoard/Controls/HomeControl.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DashBoard.Views
+namespace DashBoard.Controls
 {
     /// <summary>
     /// HomeView.xaml에 대한 상호 작용 논리
