540a05677cdcabb2bf16c1a8fe404f5e7ccd8b51
Cire <dongjun.dev@gmail.com>
2025-03-23 21:13:53 +0900

    Add ExeIconConverter class
    -

--- DashBoard/Controls/IntroLauncherControl.xaml.cs
+++ DashBoard/Controls/IntroLauncherControl.xaml.cs
@@ -3,16 +3,13 @@ using DashBoard.ViewModels;
 
 namespace DashBoard.Controls
 {
-    /// <summary>
-    /// IntroLauncherContrl.xaml에 대한 상호 작용 논리
-    /// </summary>
     public partial class IntroLauncherControl : UserControl
     {
         public IntroLauncherControl()
         {
-            InitializeComponent();
-
             this.DataContext = App.Current.Services.GetService(typeof(IntroLauncherViewModel));
+
+            InitializeComponent();
         }
     }
 }
---
+++ DashBoard/Converter/ExeIconConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace DashBoard.Converter
+{
+    class ExeIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string exePath = value as string;
+            if (!string.IsNullOrEmpty(exePath) && File.Exists(exePath))
+            {
+                Icon icon = Icon.ExtractAssociatedIcon(exePath);
+                if (icon != null)
+                {
+                    return Imaging.CreateBitmapSourceFromHIcon(
+                        icon.Handle,
+                        Int32Rect.Empty,
+                        BitmapSizeOptions.FromEmptyOptions());
+                }
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
